0fabcc5c318176644371b73db04237789a47a7b2
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-07-12 14:58:16 +0700

    updated lop.py

---
+++ 01-fundamental/loop.py
@@ -0,0 +1,14 @@
+message = "Hello World!"
+usia = 17
+
+print(message)
+print(usia)
+
+for i in range(0,3):
+    print(message)
+
+
+while usia > 0:
+    print("Usia saat ini %s" % usia)
+    print("Masih hidup!")
+    usia = usia - 1
