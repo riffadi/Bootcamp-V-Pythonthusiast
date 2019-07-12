08ab357e97e25f61c6b764a911f0049673183267
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-07-12 21:24:31 +0700

    using dictionary

---
+++ 01-fundamental/dictionary.py
@@ -0,0 +1,12 @@
+manusia = {}
+manusia['nama'] = 'Prabowo'
+manusia['sex'] = 'Laki - laki'
+manusia['status'] = 'Menikah'
+print(manusia)
+#edit atribute
+manusia['nama'] = 'Prabowo widagyo'
+print(manusia)
+#update atribute
+manusia['alamat'] = 'Jakarta'
+print(manusia)
+
