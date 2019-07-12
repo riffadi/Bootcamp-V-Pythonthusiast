975aeb4e069e727f050bc62c86cfa6d033cb0174
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-07-12 14:47:43 +0700

    add condition

---
+++ 01-fundamental/condition.py
@@ -0,0 +1,21 @@
+message = "Hello World!"
+nama = "Anda joko"
+usia = 17
+lingkar_perut = 30.5
+
+print(message)
+print(nama)
+print(usia)
+print(lingkar_perut)
+
+if usia <= 17:
+    print("Masih muda cuy!")
+else:
+    print("Mayan Tua")
+
+if lingkar_perut < 30:
+    print("Sudah bagus")
+elif lingkar_perut < 40:
+    print("Susah pakai seat belt ya?")
+else:
+    print("Bahaya!")
