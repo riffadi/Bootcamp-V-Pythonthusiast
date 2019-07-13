59793c088f18a6c4df101f5e036fff41648b9f7c
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-07-13 14:25:31 +0700

    Update all

--- 02-Modularization/models.py
+++
@@ -1,7 +0,0 @@
-daftar_teman = []
-daftar_teman.append('Joko')
-daftar_teman.append('Prabowo')
-daftar_teman.append('Mega')
-daftar_teman.append('Gareng')
-
-
--- 02-Modularization/start.py
+++ 02-Modularization/start.py
@@ -1,3 +1,6 @@
-from views import run_view
+from teman.views import run_view as run_view_teman
+from buku.views import run_view as run_view_buku
 
-run_view()
+run_view_teman()
+print('===========')
+run_view_buku()
